9381176d2756a0782e5d1ca4afc3e5e9d022da43
SalsaZishi <wuzishi@yahoo.com>
2015-11-07 22:01:36 -0500

    Update BitOps.c

--- LabTask6/BitOps.c
+++ LabTask6/BitOps.c
@@ -1,10 +1,24 @@
+//----------------------------------------------------------------------------------
+// Zishi Wu
+// October 26, 2015
+// BitOps.c
+//----------------------------------------------------------------------------------
+/* This program contains 3 macro expressons: 
+ * 1) Check if X is an odd number
+ * 2) Check if the Nth digit (starting from 0) of X is turned on (digit = 1)
+ * 3) Check if all digits of X from S to E are on 
+ 
+ * For an in-depth explanation on the 3rd macro expression, check out the 
+ * Explanations file. This task involves a lot of thinking in binary.
+ */ 
+//----------------------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
 
 #define ODD(X) ((X) & 01)
 #define BITON(X,N) ((X) & (1<<N))
 #define ALLON(X,S,E) ( ((X) & ( (1<<(E+1))-(1<<(S)) )) == ( (1<<(E+1))-(1<<(S)) ) )
-//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 int main(void) {
 
     unsigned int U1,BitNumber,Start,End;
@@ -23,4 +37,4 @@ int main(void) {
 
     return(EXIT_SUCCESS);
 }
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
